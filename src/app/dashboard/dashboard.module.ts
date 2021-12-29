fead5121d5fd04e8186db88f72fada1cc440439c
أحمد نعناع <ahmadn3na3@gmail.com>
2021-12-29 19:25:12 +0200

    ini

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { EmployeeModule } from '../employee/employee.module';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { DashboradRoutingModule } from './dashborad-routing.module';
+
+
+
+@NgModule({
+  declarations: [DashboardComponent, NavComponent],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    EmployeeModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    DashboradRoutingModule
+  ]
+})
+export class DashboardModule { }
